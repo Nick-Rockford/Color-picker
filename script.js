2f3c95285833fe9e16d9518eccd9740a38ef96b7
Nick Rockford <nwoffard@cnm.edu>
2022-06-29 13:14:59 -0600

    Initial commit to trouble shoot

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
-//initilization function
+//initialization function
 window.onload = function() {
     initColorPicker();
 };
 
 //references to our color inputs and color box.
 function initColorPicker() {
-    let colorBox = document.getElementById("color-box");
-    let rgb = {
+    let element = document.getElementById("color-box");
+    let colors = {
         red: document.getElementById("red"),
         green: document.getElementById("green"),
         blue: document.getElementById("blue")
     };
     let colorPickers = document.getElementsByClassName("picker");
-    setColorPickerEventListeners(colorBox, rgb, colorPickers);
+    setColorPickerEventListeners(element, colors, colorPickers);
 }
 // set events on each of the inputs. updated to be a single function.
-function setColorPickerEventListeners(colorBox, rgb, pickerElements){
+function setColorPickerEventListeners(element, colors, pickerElements){
     let pickerLen = pickerElements.length;
     for (let i = 0; i < pickerLen; i++){
         pickerElements[i].addEventListener('change', () =>{
-            let red = rgb.red.value;
-            let green = rgb.green.value;
-            let blue = rgb.blue.value;
+            let red = colors.red.value;
+            let green = colors.green.value;
+            let blue = colors.blue.value;
             console.clear();
             console.log("Red value: ", red, "green value: ", green, "blue value: ", blue);
-            setBoxBGColor(colorBox, red, green, blue);
+            setElementBGColor(element, red, green, blue);
             setDisplayValues(red, green, blue);
         });
     }
 /* leaving this here to help me understand whats happening. but this is legacy code.
     rgb.red.addEventListener("change", () => {
         console.log("Red value: ", rgb.red.value);
-        setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
+        setElementBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
     })
     rgb.green.addEventListener("change", () => {
         console.log("Green value: ", rgb.green.value);
-        setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
+        setElementBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
     })
     rgb.blue.addEventListener("change", () => {
         console.log("Blue value: ", rgb.blue.value);
-        setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
+        setElementBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
     })
  */
 }
 
-function setBoxBGColor (colorBox, red, green, blue){
-    rgbVal = [red, green, blue].join(',');
-    colorBox.style.backgroundColor = "rgb(" + rgbVal + ")"
+function setElementBGColor (bgElement, red, green, blue){
+    let rgbVal = [red, green, blue].join(',');
+    bgElement.style.backgroundColor = "rgb(" + rgbVal + ")"
 }
 
-function setDisplayValues(colorBox, red, green, blue){
+function setDisplayValues(red, green, blue){
     let redVal = document.getElementById("redVal");
     let greenVal = document.getElementById("greenVal");
     let blueVal = document.getElementById("blueVal")
